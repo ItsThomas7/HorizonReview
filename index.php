7758fa0a0391ee9e5004f445926f698a8fbc5507
ToiletSauce <104719@student.horizoncollege.nl>
2020-11-27 13:52:21 +0100

    Added project essentials

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Horizon Review</title>
+
+    <!-- Meta tags -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Stage studenten beoordelen">
+    <meta name="keywords" content="Horizon, Stage, Beoordelen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body>
+
+</body>
+</html>
