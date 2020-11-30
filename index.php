0a8eae438115f0e2d2bdcc42c64643cb9a425725
ToiletSauce <104719@student.horizoncollege.nl>
2020-11-30 10:35:19 +0100

    Start on index

--- index.php
+++ index.php
@@ -11,9 +11,32 @@
 
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <!--  JS  -->
+    <script src="js/main.js"></script>
+
 </head>
 
 <body>
+<header class="topnav">
+    <button class="open-login" onclick="openLogin()">Admin</button>
+    <div class="login-popup" id="loginForm">
+        <form action="#" class="form-container">
+            <h2>Inloggen</h2>
+
+            <label for="uName"><b>Naam</b></label>
+            <input type="text" placeholder="Naam" name="uName" required>
+
+            <label for="psw"><b>Wachtwoord</b></label>
+            <!-- Change to input password after testing -->
+            <input type="text" placeholder="Wachtwoord" name="psw" required>
+
+            <button type="submit" class="btn">Inloggen</button>
+            <button type="button" class="btn cancel" onclick="closeLogin()">Sluiten</button>
+        </form>
+    </div>
+    <img src="img/horizonlogo.png">
+</header>
 
 </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openLogin() {
+    document.getElementById("loginForm").style.display = "block";
+}
+
+function closeLogin() {
+    document.getElementById("loginForm").style.display = "none";
+}
