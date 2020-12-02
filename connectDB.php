ed447748d51d56d3cbc12e49b36b62f619f25f69
ItsThomas7 <56074673+ItsThomas7@users.noreply.github.com>
2020-12-02 11:33:27 +0100

    added connectDB with quotes for use

---
+++ connectDB.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=HorizonReview", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+//Zet include='conncectDB'; Om een connectie te maken met de database indien nodig op een pagina. (In php quotes <?php ? >)
+?>
