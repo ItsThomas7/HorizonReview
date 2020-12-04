69396f9387b510ad17eb702f620b51ddfbc0341b
ToiletSauce <104719@student.horizoncollege.nl>
2020-12-04 10:29:26 +0100

    added comment

--- admin.php
+++ admin.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="nl">
 <head>
+    <!-- TODO Preventie toevoegen zodat je niet op deze pagina kan komen als er niet ingelogd is -->
+
     <title>Horizon Review</title>
 
     <!-- Meta tags -->
